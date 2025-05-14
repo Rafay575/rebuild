d9536bfabe22de6d89170dab02c0b48fb8fafd45
Abdul Rafay Ahmed <111084742+Rafay575@users.noreply.github.com>
2025-05-14 23:01:56 +0500

    ///

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Jd8p0/BvoagfgJUJv91w1seO/vKkPa1pOudhGkWC2EA=\"\n}"
