c289cb142d383e931c2b2b2063bb8e18fc99f7ef
Abdul Rafay Ahmed <111084742+Rafay575@users.noreply.github.com>
2025-05-20 20:09:59 +0500

    ..

---
+++ .next/static/chunks/app_services_page_tsx_77e069._.js
@@ -0,0 +1,14 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/app_services_page_tsx_77e069._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/app_services_page_tsx_77e069._.js",
+  "chunks": [
+    "static/chunks/[root of the server]__3a24bc._.js",
+    "static/chunks/node_modules_b818b7._.js",
+    "static/chunks/[next]_internal_font_google_plus_jakarta_sans_ccad94fd_module_60b0be.css",
+    "static/chunks/components_ServicesTabs_tsx_1ce3ef._.js"
+  ],
+  "source": "dynamic"
+});
